ef1c8ddcdd597bd6ca6865d84de2f1a58ecde892
G-Parris <gilespar420@gmail.com>
2025-04-14 14:18:47 -0400

    Idk anymore

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Disable server-side rendering for GitHub Pages
-  ssr: false,
+  // Config options...
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: true,
 } satisfies Config;
