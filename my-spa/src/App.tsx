9c97e32d7126cf3d0b9e2c9970245577f313a9f8
G-Parris <gilespar420@gmail.com>
2025-04-14 18:00:25 -0400

    a

--- my-spa/src/App.tsx
+++ my-spa/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import TaskList from './routes/tasklist';
 import CreateTask from './routes/createtask';
 import Task from './routes/task';
@@ -15,6 +15,7 @@ function App() {
         <Route path="/tasks" element={<TaskList />} />
         <Route path="/tasks/new" element={<CreateTask />} />
         <Route path="/tasks/:id" element={<Task />} />
+        <Route path='react-router' element={<Navigate to="/" />} />
         <Route path='*' element={<TaskList />} />
         {/* Add a catch-all route for 404s */}
       </Routes>
--- my-spa/vite.config.ts
+++ my-spa/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/react-router/",
+  base: '/react-router/',
   plugins: [react()],
 });
